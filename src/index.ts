ccf9acca081c09d84b1d69d901fdc42e40a79c55
Fityan <fityannugroho@gmail.com>
2023-07-07 21:33:54 +0700

    feat: use ora spinner

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { confirm, input, select } from '@inquirer/prompts';
+import cliProgress from 'cli-progress';
+import ora from 'ora';
 import comparator from './comparator/index.js';
 import { crawlFromPdf } from './crawler/index.js';
 import extractor from './extractor/index.js';
-import { ProgressBar } from './utils/cli.js';
 import { Data } from './utils/types.js';
 
 const main = async () => {
@@ -22,24 +23,26 @@ const main = async () => {
   const compareData = await confirm({ message: 'Compare for data changes?', default: false });
 
   // === Start the program execution ===
-  const bar = ProgressBar();
+  const bar = new cliProgress.SingleBar({
+    format: 'Crawling data [{bar}] {percentage}% ({value}/{total} pages) in {duration_formatted}',
+    barsize: 24,
+  });
 
   if (filePath) {
     await crawlFromPdf(filePath, {
-      onStart: (totalPages) => bar.start(totalPages, 0, { label: 'Crawling data' }),
+      onStart: (totalPages) => bar.start(totalPages, 0),
       onPageCrawled: (pagesCrawled) => bar.update(pagesCrawled),
     });
 
     bar.stop();
   }
 
-  bar.start(1, 0, { label: 'Formatting data' });
+  const spinner = ora().start('Extracting data');
   extractor(dataToFormat);
-
-  bar.update(1);
-  bar.stop();
+  spinner.succeed(`${dataToFormat} data extracted`);
 
   if (compareData) {
+    console.log('Comparing data...');
     await comparator(dataToFormat);
   }
 

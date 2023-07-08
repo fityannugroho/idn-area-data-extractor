4a120a9ad092075f0d440287b5996f8d566829eb
Fityan <fityannugroho@gmail.com>
2023-07-08 18:31:41 +0700

    extract regency code

--- src/comparator/regencies.ts
+++ src/comparator/regencies.ts
@@ -1,16 +1,13 @@
-import { Regency, regencies } from 'idn-area-data';
 import { diff } from 'jest-diff';
 import diffConfig from '../config/diff.js';
-import csvParser from '../utils/csv-parser.js';
-import { outputPath } from '../utils/path.js';
+import { csvSplitter } from '../utils/csv-parser.js';
+import { latestDataPath, outputPath } from '../utils/path.js';
 
 const compareRegencies = async () => {
-  const oldRegencies = await regencies();
-  const newRegencies = await csvParser<Regency>(outputPath);
+  const { rows: oldRegencies } = await csvSplitter(latestDataPath('regencies'));
+  const { rows: newRegencies } = await csvSplitter(outputPath);
 
-  const oldRegencyNames = oldRegencies.map((regency) => regency.name);
-  const newRegencyNames = newRegencies.map((regency) => regency.name);
-  const diffRegencyNames = diff(oldRegencyNames, newRegencyNames, diffConfig);
+  const diffRegencyNames = diff(oldRegencies, newRegencies, diffConfig);
 
   console.log(diffRegencyNames);
 };
--- src/extractor/regencies.ts
+++ src/extractor/regencies.ts
@@ -1,29 +1,47 @@
 import fs from 'node:fs';
 import { inputPath, outputPath } from '../utils/path.js';
 
-// The regex was tested in https://regex101.com/r/J63CVl
-const regex = /^((?:KAB\.?|KOTA)\s[A-Z. ]+)\s/;
-
 const extractRegency = (data: string) => {
-  const matchArr = data.match(regex);
+  // The regex was tested in https://regex101.com/r/J63CVl
+  const regencyRegex = /^((?:KAB\.?|KOTA)\s[A-Z\\. ]+)\s.+(\d{2}\.\d{2})\s.+$/i;
 
-  if (!matchArr || !matchArr.length) {
+  const match = data.match(regencyRegex);
+
+  if (!match || !match.length) {
     return '';
   }
 
-  // Can not get the regency code
-  const name = matchArr[1].replace(/KAB(?:\.?)\s/, 'KABUPATEN ');
+  const name = match[1].replace(/KAB(?:\.?)\s/, 'KABUPATEN ');
+  const code = match[2].replace(/\./g, '');
+  const provinceCode = code.substring(0, 2);
 
-  return [name].join(',');
+  return [code, provinceCode, name].join(',');
 };
 
 const extractRegencies = () => {
   const input = fs.readFileSync(inputPath, 'utf-8');
-  const lines = input.trim().split('\n');
-  const res = lines.map((line) => extractRegency(line)).filter((line) => line);
-  const header = [
-    'name',
-  ];
+  const rows = input.trim().split('\n');
+  const mergedRows: string[] = [];
+
+  // Combine data spread across multiple rows.
+  for (let i = 0; i < rows.length; i += 1) {
+    const row = rows[i];
+    // The regex was tested in https://regex101.com/r/l1PJvE
+    const regencyCodeRegex = /^((?:KAB\.?|KOTA)\s[A-Z. ]+)\s/i;
+
+    if (row.match(regencyCodeRegex)) {
+      mergedRows.push(row);
+    } else {
+      const lastRow = mergedRows.pop();
+
+      if (lastRow) {
+        mergedRows.push(`${lastRow} ${row}`);
+      }
+    }
+  }
+
+  const res = mergedRows.map((row) => extractRegency(row));
+  const header = ['code', 'province_code', 'name'];
 
   // Save the result to a file
   fs.writeFileSync(outputPath, `${header.join(',')}\n${res.join('\n')}`);
